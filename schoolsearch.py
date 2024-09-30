d8d0f1666c4c726938975418f82a148ba3ad53fe
am0ren77 <amoren77@calpoly.edu>
2024-09-29 18:08:13 -0700

    Setup: Read file and create list of dictionaries

--- schoolsearch.py
+++ schoolsearch.py
@@ -1,1 +1,27 @@
-# start
+# Ashley Moreno
+# Lab 1 Part a
+# schoolsearch.py
+
+
+# Function to read students.txt file and store data in list of dictionaries
+
+def read_students(filename):
+    students = []
+    with open(filename, 'r') as file:
+	for line in file:
+	    format_parts = line.strip().split(', ')
+	    if len(format_parts) != 8:
+		print("Error: Incorrect file format. Exiting program.")
+                return None
+
+	    student = {
+		'StLastName': parts[0],
+                'StFirstName': parts[1],
+                'Grade': int(parts[2]),
+                'Classroom': int(parts[3]),
+                'Bus': int(parts[4]),
+                'GPA': float(parts[5]),
+                'TLastName': parts[6],
+                'TFirstName': parts[7]
+	    }
+	    students.append(student)
